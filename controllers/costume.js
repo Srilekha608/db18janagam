fd44172e79d3357e7f94bbd50884c95132486469
Srilekha608 <64957724+Srilekha608@users.noreply.github.com>
2021-11-17 22:30:52 -0600

    updated the new endpoints

--- controllers/costume.js
+++ controllers/costume.js
@@ -9,12 +9,19 @@ exports.costume_list = async function(req, res) {
     catch(err){ 
         res.status(500); 
         res.send(`{"error": ${err}}`); 
-    }   
+    }  
 }; 
  
 // for a specific Costume. 
-exports.costume_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id); 
+exports.costume_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Costume.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle Costume create on POST. 
@@ -39,18 +46,39 @@ exports.costume_create_post = async function(req, res) {
 }; 
  
 // Handle Costume delete form on DELETE. 
-exports.costume_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id); 
+exports.costume_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Costume.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
  
 // Handle Costume update form on PUT. 
-exports.costume_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id); 
+exports.costume_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Costume.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.costume_type)  
+               toUpdate.costume_type = req.body.costume_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 
-   
-// VIEWS 
-// Handle a show all view 
 exports.costume_view_all_Page = async function(req, res) { 
     try{ 
         theCostumes = await Costume.find(); 
@@ -61,3 +89,51 @@ exports.costume_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+exports.costume_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Costume.findById( req.query.id) 
+        res.render('costumedetail',  
+{ title: 'Costume Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+exports.costume_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('costumecreate', { title: 'Costume Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+exports.costume_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Costume.findById(req.query.id) 
+        res.render('costumeupdate', { title: 'Costume Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+exports.costume_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Costume.findById(req.query.id) 
+        res.render('costumedelete', { title: 'Costume Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
